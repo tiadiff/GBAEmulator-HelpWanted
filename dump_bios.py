631208aa0937c5b4843f39cbcb2d4eb161c46e2a
tiadiff <mattia.scalise@gmail.com>
2026-06-15 17:48:23 +0200

    Remove python helper scripts

--- dump_bios.py
+++
@@ -1,9 +0,0 @@
-import struct
-
-with open('bin/Release/net8.0-windows/rom/gba_bios.bin', 'rb') as f:
-    f.seek(0x00)
-    d = f.read(0x200)
-    for i in range(0, len(d), 4):
-        print(f"{i:08X}: {struct.unpack('<I', d[i:i+4])[0]:08X}")
-    val = struct.unpack('<I', d[i:i+4])[0]
-    print(f"{0x180 + i:03X}: {val:08X}")
